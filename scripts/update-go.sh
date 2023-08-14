096c1316294c9e8c7e4807e8376e06a73ebbd17d
galargh <piotr.galar@gmail.com>
2023-08-14 10:59:28 +0200

    fix: current go version extraction

--- scripts/update-go.sh
+++ scripts/update-go.sh
@@ -28,7 +28,7 @@ pushd "$TARGET" > /dev/null
 while read file; do
   pushd "$(dirname "$file")" > /dev/null
 
-  current="$(go list -m -f {{.GoVersion}})"
+  current="$(go list -m -f {{.GoVersion}} | tail -n-1)"
 
   if [[ "$current" == "$expected" ]]; then
     echo "Go version $expected already in use."
