a832ca5fa4ee9d19904b269b02becda04da925a4
galargh <piotr.galar@gmail.com>
2026-03-01 16:32:13 +0100

    Update aegir script

---
+++ scripts/update-aegir.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+set -euo pipefail -o nounset
+
+# If the script is run with RUNNER_DEBUG=1, print all statements executed
+if [[ "${RUNNER_DEBUG:-}" == "1" ]]; then
+  set -x
+fi
+
+language="$(jq -r '.github.languages | map(select(. == "JavaScript")) | .[0]' <<< "$CONTEXT")"
+
+if [[ "$language" != "JavaScript" ]]; then
+  echo "Not a Node project. Skipping."
+  exit 0
+fi
+
+pushd "$TARGET" > /dev/null
+if [[ ! -f .github/workflows/js-test-and-release.yml ]]; then
+  echo "No .github/workflows/js-test-and-release.yml file found. Skipping."
+  exit 0
+fi
+popd > /dev/null
+
+pushd "$TARGET" > /dev/null
+
+while read file; do
+  npm install
+  npm install "aegir@^v47.0.26" --save-dev
+
+  git add .
+
+  if ! git diff-index --quiet HEAD; then
+    git commit -m "chore(deps): update aegir to v47.0.26"
+  fi
+  popd > /dev/null
+done <<< "$(git ls-tree --full-tree --name-only HEAD | grep 'package\.json$')"
+
+popd > /dev/null
