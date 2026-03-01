f31e3c2c5e2590b804e4b6439b1d8ad8cb0757fd
galargh <piotr.galar@gmail.com>
2026-03-01 17:13:35 +0100

    no

--- scripts/update-aegir.sh
+++ scripts/update-aegir.sh
@@ -7,20 +7,6 @@ if [[ "${RUNNER_DEBUG:-}" == "1" ]]; then
   set -x
 fi
 
-language="$(jq -r '.github.languages | map(select(. == "JavaScript")) | .[0]' <<< "$CONTEXT")"
-
-if [[ "$language" != "JavaScript" ]]; then
-  echo "Not a Node project. Skipping."
-  exit 0
-fi
-
-pushd "$TARGET" > /dev/null
-if [[ ! -f .github/workflows/js-test-and-release.yml ]]; then
-  echo "No .github/workflows/js-test-and-release.yml file found. Skipping."
-  exit 0
-fi
-popd > /dev/null
-
 pushd "$TARGET" > /dev/null
 
 npm install
